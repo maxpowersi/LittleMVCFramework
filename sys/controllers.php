0b1f33bab31b1ba13a23a6028dfe752b4c3e5377
Maxpowersi <maxi1990@gmail.com>
2018-06-04 17:55:00 -0300

    New request functions
    New request functions

--- sys/controllers.php
+++ sys/controllers.php
@@ -1,69 +1,61 @@
 <?php	
 
-	//Action to not found event
-	function NotFound()
-	{
-		RenderWithView("not_found");
-	}
+//Action to not found event
+function NotFound()
+{
+    RenderWithView("not_found");
+}
 
-	//Action to error event
-	function Error($e)
-	{
-		RenderWithView("generic_error", array("msg" => $e));
-	}
+//Action to error event
+function Error($e)
+{
+    RenderWithView("generic_error", array("msg" => $e));
+}
 
-	//Get a GET parameter in a secure way
-	function GetParameterGET($parameterName)
-	{
-		return htmlentities($_GET[$parameterName]);
-	}
+//Call from a function in a controller, an pass the parameters, to set the defaul view, with default properties.
+function RenderMainView($view, $properties = array())
+{
+    foreach ($properties as $key => $value)
+        ${$key} = $value;
 
-	//Get a POST parameter in a secure way
-	function GetParameterPOST($parameterName)
-	{
-		return htmlentities($_POST[$parameterName]);
-	}
+    include "views/". $view . ".view";
+}
 
-	//Call from a function in a controller, an pass the parameters, to set the defaul view, with default properties.
-	function RenderMainView($view, $properties = array())
-	{
-		foreach ($properties as $key => $value) 
-			${$key} = $value;	
+//Render a view, sending many variables to it
+function RenderWithView($viewName, $properties = array(), $mainView = null)
+{
+    //We create in this scope, dinamic "properties"
+    foreach ($properties as $key => $value)
+        ${$key} = $value;
 
-        include "views/". $view . ".view";
-	}
+    if($mainView != null)
+    {
+        //specific view
+        ob_start();
+        include("views/".$viewName.".view");
+        $Content = ob_get_clean();
+        include "/../views/". $mainView . ".view";
+    }
+    else
+    {
+        //default view
+        ob_start();
+        include("views/".$viewName.".view");
+        $Content = ob_get_clean();
+        DefaultMainView($Content);
+    }
+}
 
-	//Render a view, sending many variables to it
-	function RenderWithView($viewName, $properties = array(), $mainView = null)
-	{
-		//We create in this scope, dinamic "properties"
-		foreach ($properties as $key => $value) 
-			${$key} = $value;		
+//Response clear text
+function ResponseClearText($clearContent)
+{
+    echo $clearContent;
+}
 
-		if($mainView != null)
-		{
-			//specific view
-			ob_start();
-			include("views/".$viewName.".view");
-			$Content = ob_get_clean();			
-			include "/../views/". $mainView . ".view";
-		}
-		else
-		{
-			//default view
-			ob_start();
-			include("views/".$viewName.".view");
-			$Content = ob_get_clean();		
-			DefaultMainView($Content);
-		}	
-	}
+//Response JSON
+function ResponseJSON($jsonString)
+{
+    echo json_encode($jsonString);
+}
 
-	function Render($viewName, $properties = array())
-	{
-		//We create in this scope, dinamic "properties"
-		foreach ($properties as $key => $value) 
-			${$key} = $value;		
-
-		include("views/".$viewName.".view");	
-	}
 ?>
--- sys/request.php
+++ sys/request.php
@@ -1,24 +1,62 @@
 <?php
+//Get JSON vars in body
+function request_GetJSONBody()
+{
+    $json = file_get_contents('php://input');
+    return json_decode($json, true);
+}
 
-    //Get the URL path after the domain
-    function request_GetRequestUri()
-    {
-        $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-        $name = basename($path);
-        return $name;
-    }
+//Get a GET parameter in a secure way
+function request_GetParameterGET($parameterName)
+{
+    if (isset($_GET[$parameterName]))
+        return htmlentities($_GET[$parameterName]);
+    else
+        return null;
+}
 
-    //Return if the request is POST
-    function request_IsPOST()
-    {
-        return $POST = $_SERVER['REQUEST_METHOD'] === 'POST';
-    }
+//Get a POST parameter in a secure way
+function request_GetParameterPOST($parameterName)
+{
+    return htmlentities($_POST[$parameterName]);
+}
+//Get the URL path after the domain
+function request_GetRequestUri()
+{
+    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    $name = basename($path);
+    return $name;
+}
 
-    //Return JSON from request content, in array format
-    function request_GetJson()
-    {
-        $inputJSON = file_get_contents('php://input');
-        $input = json_decode($inputJSON, TRUE);		
-        return $input;
-    }
+//Return if the request is POST
+function request_IsPOST()
+{
+    return $POST = $_SERVER['REQUEST_METHOD'] === 'POST';
+}
+
+//Return if the request is DELETE
+function request_IsDELETE()
+{
+    return $POST = $_SERVER['REQUEST_METHOD'] === 'DELETE';
+}
+
+//Return if the request is PUT
+function request_IsPUT()
+{
+    return $POST = $_SERVER['REQUEST_METHOD'] === 'PUT';
+}
+
+//Return if the request is GET
+function request_IsGET()
+{
+    return $POST = $_SERVER['REQUEST_METHOD'] === 'GET';
+}
+
+//Return JSON from request content, in array format
+function request_GetJson()
+{
+    $inputJSON = file_get_contents('php://input');
+    $input = json_decode($inputJSON, TRUE);
+    return $input;
+}
 ?>
